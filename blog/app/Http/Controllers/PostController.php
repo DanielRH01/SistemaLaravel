50f3dc54764856fb4ff707d37fa836f6c07d2ea7
DanielRH01 <draymundoherrera975@gmail.com>
2025-11-11 02:38:04 -0500

    Commit de la modificación de controlador

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
         return view('post.create');
     }
 
-    //Función para mostrar un post específico según el parámetro {post}
+    //Función para mostrar un post específico según el parámetro {post} ingresado en la URL
     public function show($post){
         return view('post.show', [
             'post' => $post
